d98e6997cdf29f17cb9f31ee04c8717d18eb6caa
itshio <morpheoosmatrix@gmail.com>
2018-02-03 12:24:54 +0100

    prueba en casa

--- app/src/main/java/com/example/franciscojavier/ejercicioencasacontroles/MainActivity.java
+++ app/src/main/java/com/example/franciscojavier/ejercicioencasacontroles/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.franciscojavier.ejercicioencasacontroles;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -42,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         rgrouptamano= (RadioGroup)findViewById(R.id.rgroup_tamano);
 
         txt_masa= (TextView)findViewById(R.id.txt_masa_inferior);
-        txt_ingredientes= (TextView)findViewById(R.id.txt_ingredientes);
+        txt_ingredientes= (TextView)findViewById(R.id.txt_ingredientes_inferior);
         txt_tamano= (TextView)findViewById(R.id.txt_tamano_inferior);
 
         String[] tipos={"Elige Masa","fina","Gruesa","Bordes rellenos","Sin bordes"};
@@ -103,7 +105,95 @@ public class MainActivity extends AppCompatActivity {
         String tipomasa= spmasa.getSelectedItem().toString();
 
         if(tipomasa.equals("Elige Masa")){
+
             Toast.makeText(this,"Debes selccionar una masa",Toast.LENGTH_LONG).show();
+
+
+
+        }else{
+
+            txt_masa.setText(tipomasa);
+            btningredientes.setEnabled(true);
+        }
+
+
+
+
+
+    }
+
+
+    public void clickingredientes (View view){
+
+        String ingredientes="";
+
+        if (cbbacon.isChecked()){
+
+            ingredientes+="Bacon, ";
+        }
+
+        if (cbternera.isChecked()){
+
+            ingredientes+="Ternera, ";
+        }
+
+        if (cbcebolla.isChecked()){
+
+            ingredientes+="Cebolla, ";
+        }
+
+        if (cbchampinones.isChecked()){
+
+            ingredientes+="Champiñones, ";
+        }
+
+        if (cbpollo.isChecked()){
+
+            ingredientes+="Pollo, ";
+        }
+
+        if (cbpeperoni.isChecked()){
+
+            ingredientes+="Peperoni, ";
+        }
+
+        txt_ingredientes.setText(ingredientes);
+
+
+    }
+
+
+    public void clicktamano (View view){
+
+        if(txt_ingredientes.equals("")){
+
+            Toast.makeText(this,"Debes selccionar los ingredientes",Toast.LENGTH_LONG).show();
+
+
+        }else{
+
+            btntamano.setEnabled(true);
+        }
+
+
+        int idradio= rgrouptamano.getCheckedRadioButtonId();
+
+        if (idradio == -1) {
+            Toast.makeText(this, "Debes selccionar un tamaño", Toast.LENGTH_LONG).show();
+
+        }else{
+
+            RadioButton radiopequeña= (RadioButton)findViewById(R.id.radiobt_pequeña);
+            String textradiopequeña= radiopequeña.getText().toString();
+
+            RadioButton radiomediana= (RadioButton)findViewById(R.id.radiobt_mediana);
+            String textradiomediana= radiomediana.getText().toString();
+
+            RadioButton radiogrande= (RadioButton)findViewById(R.id.radiobtn_grande);
+            String taxtradiogrande= radiogrande.getText().toString();
+
+
+
 
         }
 
@@ -112,7 +202,16 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    }
+
+
+
+
+
+
+
+
+
+        }
 
 
 
